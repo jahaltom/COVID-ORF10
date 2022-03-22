87edd98e7981e898b776a78c1bccdc4789c204fd
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-03-22 11:20:51 -0500

    Create DESeq2.r

---
+++ DESeq2.r
@@ -0,0 +1,29 @@
+library(DESeq2)
+
+#Read in count information.
+countData = read.table("results_Count_gene.tsv",header=TRUE,row.names=1,sep = '\t')
+#Round to nearest int
+countData=round(countData,0)
+##Read in expermental design
+metadata = read.table("Design.tsv",header=TRUE,row.names=1,sep = '\t')
+
+
+#Get sample names
+samples=row.names(metadata)
+
+
+#Should return TRUE
+#all(rownames(metadata) == colnames(countData))
+
+##Make DEseq2 object
+dds = DESeqDataSetFromMatrix(countData = countData,colData = metadata,design = ~ Sample)
+dds = DESeq(dds)
+#Contrast case vs control
+result = results(dds, contrast=c("Sample","EVC","WT"))
+## Remove rows with NA
+result = result[complete.cases(result),]
+#Put GeneID as column
+result = cbind(miRNA_ID = rownames(result), result)
+
+
+write.table(result,"EVCvsWT_DGE.tsv" ,sep = '\t',row.names = FALSE)
