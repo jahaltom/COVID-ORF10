862e771083e68b2265a32c896656f5e5c1c57108
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-04-03 16:08:13 -0500

    Update SingleStudy_Filter.py

--- SingleStudy_Filter.py
+++ SingleStudy_Filter.py
@@ -11,11 +11,11 @@ df_gene_count = pd.read_csv("results_Count_gene.tsv",sep='\t')
 
 #create a median column
 df_gene_tpm['median']=df_gene_tpm.median(axis=1)
-#Remove EB genes where TPM median < 1 
+#Remove EB genes where TPM median < 1
 indexNames = df_gene_tpm[(df_gene_tpm['median'] < 1) & (df_gene_tpm['Gene_type'] == 'EB_novel') ].index
 df_gene_tpm.drop(indexNames , inplace=True)
 
-#Gather list of Gene_stable_IDs from filtered TPM an use to filter counts. 
+#Gather list of Gene_stable_IDs from filtered TPM an use to filter counts.
 id_list=df_gene_tpm['Gene_stable_ID'].tolist()
 df_gene_count=df_gene_count[df_gene_count['Gene_stable_ID'].isin(id_list)]
 
@@ -28,5 +28,5 @@ df_gene_count=df_gene_count[df_gene_count['Gene_stable_ID'].isin(id_list)]
 #indexNames = df_gene_tpm[(df_gene_tpm['Gene_type'] == 'EB_novel') & (df_gene_tpm['median'] < med_med_lnc) ].index
 #df_gene_tpm.drop(indexNames , inplace=True)
 
-df_gene_tpm.to_csv('TPM.filtered.tsv',sep='\t',index=False)     
-df_gene_count.to_csv('Count.filtered.tsv',sep='\t',index=False)   
+df_gene_tpm.to_csv('TPM.filtered.tsv',sep='\t',index=False)
+df_gene_count.to_csv('Count.filtered.tsv',sep='\t',index=False)
